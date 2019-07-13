8d9522fcf9ce0ae48fcdf26311066982d7cdfb82
rglepe <rglepe@gmail.com>
2019-07-13 19:36:47 +0200

    changed to json-server-auth

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
  
-const app = jsonServer.create()
-const router = jsonServer.router('db.json')
+const app = jsonServer.create();
+const router = jsonServer.router('db.json');
  
 // /!\ Bind the router db to the app
 app.db = router.db;
