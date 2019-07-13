90548c1cea1662ab206daa68d621d2e0efb6bc32
rglepe <rglepe@gmail.com>
2019-07-13 19:23:03 +0200

    changed to json-server-auth

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
+const auth = require('json-server-auth');
+ 
+const app = jsonServer.create()
+const router = jsonServer.router('db.json')
+ 
+// /!\ Bind the router db to the app
+app.db = router.db;
+ 
+// You must apply the auth middleware before the router
+app.use(auth);
+app.use(router);
+app.listen(3000);
+/* const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
@@ -7,4 +19,4 @@ const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port); */
